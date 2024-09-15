6e65dd30fdddb8eef9b62a94762dd6ae6f03ecbe
Suvo-Paul <suvopaul098@gmail.com>
2024-09-15 16:38:19 +0530

    02counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(5)
+
+  // let counter = 15
+
+  const increaseValue = () => {
+    if (counter < 10) {
+      setCounter(counter + 1)
+    }
+  }
+
+  const decreaseValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Hello React</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={increaseValue}>Increase value {counter}</button>
+      <br /><br />
+      <button onClick={decreaseValue}>Decrease value {counter}</button>
+      <p>Footer : {counter}</p>
+    </>
+  )
+}
+
+export default App
