5246901a953ab59630969549ea48997279f18ff6
Suvo-Paul <suvopaul098@gmail.com>
2024-09-16 18:07:42 +0530

    changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
-  const [counter, setCounter] = useState(5)
+  const [counter, setCounter] = useState(15);
 
   // let counter = 15
 
   const increaseValue = () => {
-    if (counter < 10) {
-      setCounter(counter + 1)
-    }
-  }
+    // if (counter < 10) {
+    //   setCounter(counter + 1)
+    // }
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+
+  };
 
   const decreaseValue = () => {
     if (counter > 0) {
-      setCounter(counter - 1)
+      setCounter(counter - 1);
     }
-  }
+  };
 
   return (
     <>
@@ -27,11 +31,12 @@ function App() {
       <h2>Counter value : {counter}</h2>
 
       <button onClick={increaseValue}>Increase value {counter}</button>
-      <br /><br />
+      <br />
+      <br />
       <button onClick={decreaseValue}>Decrease value {counter}</button>
       <p>Footer : {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
