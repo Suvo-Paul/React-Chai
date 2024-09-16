3a59eb92d74a7f9aa1b19045b238b0e1d39244d7
Suvo-Paul <suvopaul098@gmail.com>
2024-09-16 14:09:14 +0530

    gg

--- 03tailwindprops/src/components/MacBookCard.jsx
+++ 03tailwindprops/src/components/MacBookCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MacBookCard = () => {
     return (
-        <div class="w-[300px] rounded-md border">
+        <div class="w-[300px] rounded-md border mb-4">
             <img
                 src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
                 alt="Laptop"
