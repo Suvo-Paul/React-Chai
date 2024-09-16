e790cda3b8963c50834fc0b4eac6e9f00717a742
Suvo-Paul <suvopaul098@gmail.com>
2024-09-16 14:08:22 +0530

    hgeh

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind Css</h1>
+      
       <Card name="Suvo" btnText="View Profile" />
 
       <MacBookCard />
